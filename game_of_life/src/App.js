3f46b0f7634648c751517737fbeab7cb8a51812a
CarlosETurcios <turcios50@gmail.com>
2020-10-19 20:12:17 -0700

    added styling, not to start working on the grid and simulation

--- game_of_life/src/App.js
+++ game_of_life/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Conway's Game of Life
       </header>
+      <div className="top">
+      <h1>About this Game</h1>
+      <p>The Game of Life is not your typical computer game. It is a 'cellular automaton', and was invented by Cambridge mathematician John Conway.
+         This game became widely known when it was mentioned in an article published by Scientific American in 1970. It consists of a collection of cells which, based on a few mathematical rules, can live, die or multiply. Depending on the initial conditions, the cells form various patterns throughout the course of the game.
+      </p>
+      </div>
+      <h1>The Simulation</h1>
+      
+
     </div>
   );
 }
